aefe9ea5b82dabbbcd5458b0c7e51820e9ced40a
Kevin Markham <justmarkham@gmail.com>
2014-09-19 18:59:01 -0400

    add code comments and better formatting

--- server.R
+++ server.R
@@ -10,35 +10,49 @@ bn.alltime <- readRDS("bn.alltime.rds")
 
 shinyServer(function(input, output){
     
+    # create reactive functions to filter data by input name
     namedata <- reactive(bn %>% filter(lname==tolower(input$name)))
     topyeardata <- reactive(bn.topyear %>% filter(lname==tolower(input$name)))
     alltimedata <- reactive(bn.alltime %>% filter(lname==tolower(input$name)))
     
+    # render the plot
     output$namePlot <- renderPlot({
         ggplot(namedata()) +
             aes(x=year, y=n, color=sex) +
             geom_line() +
             theme_bw() +
-            theme(legend.position=c(0,1), legend.justification=c(0,1), legend.title=element_blank(), legend.background=element_blank()) +
+            theme(legend.position=c(0,1), legend.justification=c(0,1),
+                  legend.title=element_blank(),
+                  legend.background=element_blank()) +
             scale_colour_discrete(labels=c("Female","Male")) +
-            ggtitle(paste0("Number of children born with the name \"", input$name, "\"")) + 
+            ggtitle(paste0("Number of children born with the name \"",
+                           input$name, "\"")) + 
             xlab("Birth Year") +
             ylab("") +
             xlim(input$year[1], input$year[2])
     })
     
+    # render "quick facts" header text
     output$text0 <- renderText({
         paste0("Quick facts about ", input$name)
     })
     
+    # render "most popular year" text
     output$text1 <- renderText({
         validate(need(tolower(input$name) %in% bn.unique, label="Valid name"))
-        paste0("The most popular year for \"", input$name, "\" was ", topyeardata()$year, ", when ", format(topyeardata()$count, big.mark=","), " children were born with that name.")
+        paste0("The most popular year for \"", input$name, "\" was ",
+               topyeardata()$year, ", when ",
+               format(topyeardata()$count, big.mark=","),
+               " children were born with that name.")
     })
-        
+    
+    # render "all-time births" text
     output$text2 <- renderText({
         validate(need(tolower(input$name) %in% bn.unique, label="Valid name"))
-        paste0(format(alltimedata()$total, big.mark=","), " children (", format(alltimedata()$F, big.mark=","), " females and ", format(alltimedata()$M, big.mark=","), " males) have been born with that name since 1880.")
+        paste0(format(alltimedata()$total, big.mark=","), " children (",
+               format(alltimedata()$F, big.mark=","), " females and ",
+               format(alltimedata()$M, big.mark=","),
+               " males) have been born with that name since 1880.")
     })
 
 })
--- ui.R
+++ ui.R
@@ -7,28 +7,56 @@ shinyUI(fluidPage(
     sidebarLayout(
         
         sidebarPanel(
+            
+            # TOP SECTION OF SIDEBAR
+            
             h4("Instructions:"),
+            
             textInput("name", label="Enter your first name:", value="Kevin"),
-            sliderInput("year", "Select years to display:", min=1880, max=2013, value=c(1880, 2013), format="####"),
+            
+            sliderInput("year", "Select years to display:", min=1880,
+                        max=2013, value=c(1880, 2013), format="####"),
             hr(),
+            
+            # BOTTOM SECTION OF SIDEBAR
+            
             h4("About:"),
-            p("This application plots the number of children born in the US each year (1880-2013), separated by gender. Data is provided by the ", a("Social Security Administration", href="http://www.ssa.gov/oact/babynames/limits.html"), " and was converted into an ", a("R package", href="https://github.com/hadley/babynames"), "by Hadley Wickham."),
+            p("This application plots the number of children born in the US 
+              each year (1880-2013), separated by gender. Data is provided by 
+              the ", a("Social Security Administration",
+                       href="http://www.ssa.gov/oact/babynames/limits.html"),
+              " and was converted into an ",
+              a("R package", href="https://github.com/hadley/babynames"),
+              "by Hadley Wickham."),
+            
             h4("External Links:"),
             tags$ul(
-                tags$li(a("Top 10 names for 2013", href="http://www.ssa.gov/oact/babynames/index.html")),
-                tags$li(a("Change in popularity (2012-2013)", href="http://www.ssa.gov/oact/babynames/rankchange.html")),
-                tags$li(a("Popular names by state", href="http://www.ssa.gov/oact/babynames/state/index.html"))
+                tags$li(a("Top 10 names for 2013",
+                          href="http://www.ssa.gov/oact/babynames/index.html")),
+                tags$li(a("Change in popularity (2012-2013)",
+                          href="http://www.ssa.gov/oact/babynames/rankchange.html")),
+                tags$li(a("Popular names by state",
+                          href="http://www.ssa.gov/oact/babynames/state/index.html"))
             ),
+            
             h4("Development:"),
-            p("Created by ", a("Kevin Markham", href="http://www.dataschool.io"), " using ", a("Shiny", href="http://shiny.rstudio.com/"), " and hosted on ", a("ShinyApps.io", href="https://www.shinyapps.io")),
+            p("Created by ", a("Kevin Markham", href="http://www.dataschool.io"),
+              " using ", a("Shiny", href="http://shiny.rstudio.com/"),
+              " and hosted on ", a("ShinyApps.io", href="https://www.shinyapps.io")),
             tags$ul(
-                tags$li(a("Source Code", href="https://github.com/justmarkham/babynames")),
-                tags$li(a("Slides", href="http://justmarkham.github.io/babynames/"))
+                tags$li(a("Source Code",
+                          href="https://github.com/justmarkham/babynames")),
+                tags$li(a("Slides",
+                          href="http://justmarkham.github.io/babynames/"))
             )
         ),
         
         mainPanel(
+            
+            # PLOT
             plotOutput("namePlot"),
+            
+            # QUICK FACTS
             h3(textOutput("text0")),
             textOutput("text1"),
             br(),
@@ -38,4 +66,3 @@ shinyUI(fluidPage(
         )
 
 ))
-
